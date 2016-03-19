2aad11f522713a9f2d578d88f7a37e2809b69acc
Robotics <Robotics@Robotics-PC>
2016-03-19 10:14:26 -0400

    Auto and turn scaling

--- theta_2016/src/org/usfirst/frc/team2363/robot/commands/autonomous/AutoRockWall.java
+++ theta_2016/src/org/usfirst/frc/team2363/robot/commands/autonomous/AutoRockWall.java
@@ -14,7 +14,7 @@ public class AutoRockWall extends DriveStraightCommand {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivetrain.arcadeDrive(.8, getCorrectedTurn());
+    	Robot.drivetrain.arcadeDrive(.85, getCorrectedTurn());
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- theta_2016/src/org/usfirst/frc/team2363/robot/subsystems/Climber.java
+++ theta_2016/src/org/usfirst/frc/team2363/robot/subsystems/Climber.java
@@ -97,7 +97,7 @@ public class Climber extends Subsystem {
     }
 
     public void initDefaultCommand() {
-        setDefaultCommand(new ClimberNothingCommand());
+        setDefaultCommand(new ClimberOverrideCommand());
     }
     
     public double getClimberCurrent() {
