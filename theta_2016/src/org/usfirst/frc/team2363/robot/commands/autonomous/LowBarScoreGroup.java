7e72fe4d6494b4b6a504fc27d62927f5c7c59719
Robotics <Robotics@Robotics-PC>
2016-04-27 20:50:16 -0400

    update score auto

--- theta_2016/src/org/usfirst/frc/team2363/robot/commands/autonomous/LowBarScoreGroup.java
+++ theta_2016/src/org/usfirst/frc/team2363/robot/commands/autonomous/LowBarScoreGroup.java
@@ -4,6 +4,8 @@ import org.usfirst.frc.team2363.robot.commands.drivetrain.BrakeCommand;
 import org.usfirst.frc.team2363.robot.commands.drivetrain.RotateTo60;
 import org.usfirst.frc.team2363.robot.commands.intake.IntakeMovement;
 import org.usfirst.frc.team2363.robot.commands.intake.IntakePosition;
+import org.usfirst.frc.team2363.robot.commands.shooter.ShooterCommand;
+import org.usfirst.frc.team2363.robot.commands.shooter.ShooterHoodCommand;
 import org.usfirst.frc.team2363.robot.subsystems.Intake.IntakeState;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -21,12 +23,17 @@ public class LowBarScoreGroup extends CommandGroup {
     	addSequential(new BrakeCommand(true));
     	addSequential(new WaitCommand(0.5));
     	addSequential(new BrakeCommand(false));
+    	addParallel(new ShooterHoodCommand(true));
+    	addParallel(new IntakePosition(true));
     	addSequential(new RotateTo60());
     	addSequential(new BrakeCommand(true));
     	addSequential(new WaitCommand(0.5));
     	addSequential(new BrakeCommand(false));
     	addSequential(new DriveStraightToTargetCommand(), 5);
     	addSequential(new BrakeCommand(true));
+    	addParallel(new ShooterCommand(true));
+    	addSequential(new WaitCommand(2));
+    	addSequential(new IntakeMovement(IntakeState.IN));
 //    	addSequential(new IntakeMovement(IntakeState.OUT));
     }
 }
